f29b3bbfb3c1e746513e1d8031ff2339179af71e
elliotwoods <elliot.woods@gmail.com>
2011-11-17 16:30:08 +0900

    fixed

--- ofx~=.h
+++ ofx~=.h
@@ -1,1 +1,5 @@
-/* @elliotwoods: @JGL @obviousjim ofx~=.h : */ ofxApproxEqual<T>(T x, T y, T sigma) { return y+sigma >= x && y-sigma <= x; }
+#pragma once
+/* @elliotwoods: @JGL @obviousjim ofx~=.h : */
+template<typename T>
+bool ofxApproxEqual(T x, T y, T sigma)
+{ return y+sigma >= x && y-sigma <= x; }
